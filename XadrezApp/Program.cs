3dd6635b1ee9f8b84eb9082c1d2626958af21eae
jocm <jose.leite@gft.com>
2021-01-07 13:43:19 -0300

    Inicio Projeto Xadrez

---
+++ XadrezApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XadrezApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
